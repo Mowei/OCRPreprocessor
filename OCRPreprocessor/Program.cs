b6b033344b53e78e9b734f1b16350dc8b076cf0d
Mowei <mowei.tw@gmail.com>
2025-04-09 16:13:34 +0800

    add

--- OCRPreprocessor/Program.cs
+++ OCRPreprocessor/Program.cs
@@ -3,6 +3,50 @@
 
 class OCRPreprocessor
 {
+    static Mat RemoveBackground(Mat gray)
+    {
+        // 轉灰階
+        //Mat gray = new Mat();
+        //Cv2.CvtColor(gray, gray, ColorConversionCodes.BGR2GRAY);
+
+        //銳化字體（可選）：
+        //Mat fonted = new Mat();
+        //Cv2.Laplacian(gray, fonted, MatType.CV_8U);
+
+        // 高斯模糊去雜訊 ---
+        Mat blurred = new Mat();
+        Cv2.GaussianBlur(gray, blurred, new Size(5, 5), 0);
+
+        /*
+        // 估計背景（用模糊模擬背景）
+        Mat background = new Mat();
+        Cv2.GaussianBlur(fonted, background, new Size(25, 25), 0);
+
+        // 背景減法
+        Mat diff = new Mat();
+        Cv2.Absdiff(fonted, background, diff);
+
+        // 對比拉伸
+        Mat norm = new Mat();
+        Cv2.Normalize(diff, norm, 0, 255, NormTypes.MinMax);
+
+
+
+        //膨脹 + 開運算 去除雜點
+        Mat cleaned = new Mat();
+        Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+        //Cv2.MorphologyEx(norm, cleaned, MorphTypes.Open, kernel);
+        */
+
+        // 二值化
+        Mat binary = new Mat();
+        //Cv2.Threshold(blurred, binary, 0, 255, ThresholdTypes.Binary | ThresholdTypes.Otsu);
+        //自動二值化
+        Cv2.AdaptiveThreshold(blurred, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, 15, 10);
+
+        return binary;
+    }
+
     static void Main(string[] args)
     {
         string inputDir = @"C:\Users\182798\Desktop\ch3_convertedToImages\Cut";    // 圖片來源資料夾
@@ -21,13 +65,17 @@ class OCRPreprocessor
 
             Mat src = Cv2.ImRead(file, ImreadModes.Grayscale);
 
+
+            Mat binary = new Mat();
+            binary = RemoveBackground(src);
+
             // --- 1. 高斯模糊去雜訊 ---
-            Mat blurred = new Mat();
-            Cv2.GaussianBlur(src, blurred, new Size(5, 5), 0);
+            //Mat blurred = new Mat();
+            //Cv2.GaussianBlur(src, blurred, new Size(5, 5), 0);
 
             // --- 2. 自動二值化 ---
-            Mat binary = new Mat();
-            Cv2.AdaptiveThreshold(blurred, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, 15, 10);
+            //Mat binary = new Mat();
+            //Cv2.AdaptiveThreshold(blurred, binary, 255, AdaptiveThresholdTypes.MeanC, ThresholdTypes.BinaryInv, 15, 10);
 
             // --- 3. 傾斜矯正 ---
             //Mat deskewed = Deskew(binary);
